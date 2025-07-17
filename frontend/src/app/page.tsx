3d25087e0f2c63530988809be0db3ba2fb0c4fdb
enzomendez315 <enzo.mendez315@gmail.com>
2025-07-17 00:12:53 -0600

    Create basic UI for file upload.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,48 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { Button } from "~/components/ui/button";
 
 export default function HomePage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const file = event.target.files?.[0];
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      
+    <main className="min-h-screen bg-stone-50 p-8">
+      <div className="mx-auto max-w-[60%]">
+        <div className="mb-12 text-center">
+          <h1 className="mb-4 text-4xl font-light tracking-tight text-stone-900">
+            CNN Audio Visualizer
+          </h1>
+          <p className="text-md mb-8 text-stone-600">
+            Upload a WAV file to see the model's predictions and feature maps.
+          </p>
+          <div className="flex flex-col items-center">
+            <div className="relative inline-block">
+              <input
+                type="file"
+                accept=".wav"
+                id="file-upload"
+                className="absolute inset-0 w-full cursor-pointer opacity-0"
+              />
+              <Button
+                disabled={isLoading}
+                className="border-stone-300"
+                variant="outline"
+                size="lg"
+              >
+                {isLoading ? "Analyzing..." : "Choose File"}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
