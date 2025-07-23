39ccd869dc4110b3fa34fdc86aef1ae6f06a44be
enzomendez315 <enzo.mendez315@gmail.com>
2025-07-22 22:22:43 -0600

    Call endpoint from frontend and upload file.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,15 +2,99 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
+import { Card, CardContent } from "~/components/ui/card";
+
+interface Prediction {
+  category: string;
+  confidence: number;
+}
+
+interface LayerData {
+  shape: number[];
+  values: number[][];
+}
+
+interface VisualizationData {
+  [layerName: string]: LayerData;
+}
+
+interface WaveformData {
+  values: number[];
+  sampleRate: number;
+  duration: number;
+}
+
+interface ApiResponse {
+  prediction: Prediction[];
+  visualization: VisualizationData;
+  inputSpectogram: LayerData;
+  waveform: WaveformData;
+}
 
 export default function HomePage() {
+  const [visualizationData, setVisualizationData] =
+    useState<ApiResponse | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [fileName, setFileName] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const handleFileChange = async (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
     const file = event.target.files?.[0];
+
+    if (!file) {
+      return;
+    }
+
+    setFileName(file.name);
+    setIsLoading(true);
+    setError(null);
+    setVisualizationData(null);
+
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = async () => {
+      try {
+        // Convert binary data into base64 encoder string
+        const arrayBuffer = reader.result as ArrayBuffer;
+        const base64String = btoa(
+          new Uint8Array(arrayBuffer).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            "",
+          ),
+        );
+
+        const response = await fetch(
+          "https://enzomendez315--audio-cnn-inference-audioclassifier-inference.modal.run",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ audio_data: base64String }),
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error(`API error ${response.statusText}`);
+        }
+
+        const data: ApiResponse = await response.json();
+        setVisualizationData(data);
+      } catch (error) {
+        setError(
+          error instanceof Error ? error.message : "An uknown error occured",
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    reader.onerror = () => {
+      setError("Failed to read the file");
+      setIsLoading(false);
+    };
   };
 
   return (
@@ -29,6 +113,8 @@ export default function HomePage() {
                 type="file"
                 accept=".wav"
                 id="file-upload"
+                onChange={handleFileChange}
+                disabled={isLoading}
                 className="absolute inset-0 w-full cursor-pointer opacity-0"
               />
               <Button
@@ -40,8 +126,25 @@ export default function HomePage() {
                 {isLoading ? "Analyzing..." : "Choose File"}
               </Button>
             </div>
+
+            {fileName && (
+              <Badge
+                variant="secondary"
+                className="mt-4 bg-stone-200 text-stone-700"
+              >
+                {fileName}
+              </Badge>
+            )}
           </div>
         </div>
+
+        {error && (
+          <Card className="mb-8 border-red-200 bg-red-50">
+            <CardContent>
+              <p className="text-red-600">Error: {error}</p>
+            </CardContent>
+          </Card>
+        )}
       </div>
     </main>
   );
